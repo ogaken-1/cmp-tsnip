827a89d7547e71363d6f34e7e2a6f0c791c93318
Kento Ogata <k.ogata1013@gmail.com>
2022-12-20 19:19:56 +0900

    Initial implement

---
+++ README.md
@@ -0,0 +1,20 @@
+# cmp-tsnip
+
+[tsnip.nvim](https://github.com/yuki-yano/tsnip.nvim) source for [nvim-cmp](https://github.com/hrsh7th/nvim-cmp).
+
+## Requirements
+
+- `nvim-cmp`
+- `denops.vim`
+- `tsnip.nvim`
+
+## Setup
+
+```lua
+require 'cmp'.setup {
+  sources = {
+    { name = 'tsnip' }
+  }
+}
+```
+
---
+++ lua/cmp-tsnip/init.lua
@@ -0,0 +1,48 @@
+local source = {}
+
+function source.new()
+  return setmetatable({}, { __index = source })
+end
+
+function source.is_available()
+  return vim.fn['denops#plugin#is_loaded'] 'tsnip' == 1
+end
+
+function source.get_debug_name()
+  return 'tsnip'
+end
+
+function source.get_keyword_pattern()
+  return '.'
+end
+
+---@class tsnip.Item
+---@field public word string
+---@field public info string
+
+function source:complete(_, callback)
+  ---@type tsnip.Item[]
+  local items = vim.fn['tsnip#items']()
+
+  local completion_items = {}
+
+  for _, item in ipairs(items) do
+    table.insert(completion_items, {
+      label = item.word,
+      filterText = item.word,
+      documentation = item.info,
+    })
+  end
+
+  callback(completion_items)
+end
+
+---@param completion_item lsp.CompletionItem
+---@param callback fun(completion_item: lsp.CompletedItem|nil)
+function source:execute(completion_item, callback)
+  vim.fn['tsnip#remove_suffix_word'](completion_item.label)
+  vim.cmd.TSnip { completion_item.label }
+  callback(completion_item)
+end
+
+return source
